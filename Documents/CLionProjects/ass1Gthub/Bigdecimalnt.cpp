ec75e61a3e07da25d14297dc5f7c787abd3a740b
salma ameer <salmaameer409@gmail.com>
2022-10-12 22:16:22 +0200

    initial commit

---
+++ Documents/CLionProjects/ass1Gthub/Bigdecimalnt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Salma ameer on 12/10/2022.
+//
+
+#include "Bigdecimalnt.h"
---
+++ Documents/CLionProjects/ass1Gthub/Bigdecimalnt.h
@@ -0,0 +1,14 @@
+//
+// Created by Salma ameer on 12/10/2022.
+//
+
+#ifndef ASS1GTHUB_BIGDECIMALNT_H
+#define ASS1GTHUB_BIGDECIMALNT_H
+
+
+class Bigdecimalnt {
+
+};
+
+
+#endif //ASS1GTHUB_BIGDECIMALNT_H
